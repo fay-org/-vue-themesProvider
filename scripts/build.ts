9e505160d272db9b7c64322f93789a2cae353f2b
kanno <812137533@qq.com>
2022-01-15 16:24:41 +0800

    refacotr: refactor bundle

---
+++ scripts/build.ts
@@ -0,0 +1,53 @@
+import fs from "fs-extra";
+import path from "path";
+import { render } from "less";
+import postcss from "postcss";
+import CleanCss from "clean-css";
+const cleanCss = new CleanCss();
+
+const styleDir = path.join(process.cwd(), "src");
+
+const transfromLess = async (code: string, filePath: string) => {
+  try {
+    const { css } = await render(code, {
+      filename: filePath,
+      //   paths: [path.join(process.cwd(), "node_modules")],
+    });
+    return css;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const tranasfromCss = async (code: string) => {
+  try {
+    const { css } = await postcss([require("autoprefixer")]).process(code, {
+      from: undefined,
+    });
+    return cleanCss.minify(css).styles;
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const generatorCssFile = (code: string, path: string) => {
+  path = path.replace(".less", ".css");
+  return fs.outputFileSync(path, code);
+};
+
+(async () => {
+  const styles = fs
+    .readdirSync(styleDir, "utf-8")
+    .map((_) => path.join(styleDir, _));
+
+  for (const style of styles) {
+    const baseName = path.basename(style);
+    if (baseName !== "common.less") break;
+    const code = await fs.readFile(style, "utf-8");
+    const source = await transfromLess(code, style);
+    const css = await tranasfromCss(source);
+    generatorCssFile(css, style);
+  }
+})();
---
+++ vite.config.ts
@@ -0,0 +1,4 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({});
